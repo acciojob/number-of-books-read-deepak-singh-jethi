588e8704e629c9c505b56b03b4be329054c84429
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-08-12 11:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,14 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count = 0;
+	for(let i  of library){
+		if(i[readingStatus]==true){
+			count++;
+		}
+		
+	}
+	return count;
 };
 
 // Do not change the code below
